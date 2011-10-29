82b4884e833b00b03c29ab9a501896de4f6b893f
Rob <thioz@mint>
2011-10-29 12:42:10 +0200

    test google maps

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 // 
 //  --- our app behavior logic ---
 //
+var gMap;
+
 run(function () {
     // immediately invoked on first run
     var init = (function () {
@@ -30,16 +32,12 @@ run(function () {
         store.get('config', function (saved) {
             // construct a gmap str
             var map  = saved ? saved.map || ui('map') : ui('map')
-            ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
-            ,   path = "http://maps.google.com/maps/api/staticmap?center=";
+            ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom');
 			
             navigator.geolocation.getCurrentPosition(function (position) {
                 var location = "" + position.coords.latitude + "," + position.coords.longitude;
-                path += location + "&zoom=" + zoom;
-                path += "&size=250x250&maptype=" + map + "&markers=color:red|label:P|";
-                path += location + "&sensor=false";
+								mapInit(position);
 
-                x$('img#static_map').attr('src', path);
             }, function () {
                 x$('img#static_map').attr('src', "assets/img/gpsfailed.png");
             });
@@ -54,3 +52,20 @@ run(function () {
         display('#welcome');
     });
 });
+
+
+function mapInit(position)
+{
+	posCenter =  new google.maps.LatLng( position.coords.latitude ,position.coords.longitude );
+	var mapOptions = {
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		center: posCenter
+	}
+
+	gMap = new google.maps.Map(document.getElementById("map_image"), mapOptions);
+
+	var m = new google.maps.Marker();
+	m.setPosition(pnt);
+	gMap.addOverlay(m);
+
+}
